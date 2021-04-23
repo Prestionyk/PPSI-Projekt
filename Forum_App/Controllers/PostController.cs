a378f9cb71eeb88af9a4edd795c851cad768a493
Mariusz Skuza <marsku8@wp.pl>
2021-04-23 19:04:05 +0200

    Show and Add Thread

---
+++ Forum_App/Controllers/PostController.cs
@@ -0,0 +1,104 @@
+﻿using Forum_App.Data;
+using Forum_App.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum_App.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PostController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: PostController
+        public ActionResult Index()
+        {
+            IEnumerable<Thread> objList = _db.Thread;
+
+            return View(objList);
+        }
+
+        // GET: PostController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PostController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Thread newItem)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Post.Add(newItem);
+                    _db.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
